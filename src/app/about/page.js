4c0974bc222a4d95fa21bebe83a90b4c8d8ce477
Georgeshayek <georgeshayek85@gmail.com>
2024-02-20 09:36:00 +0200

    about update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,14 @@
 export default function About(){
 
-    return(<h1>hey whats up homie</h1>)
+    return(<><h1>Welcome to our website</h1>
+    
+    <p className={styles.text}> 
+				Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+				accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+				illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+				explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+				od
+			</p>
+    </>)
     
 }
